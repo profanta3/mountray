91bc28bc1939ea67c5ad6caa95f34eb35746ff1c
Karl Blumenthal <me@profanta.de>
2022-02-02 21:42:26 +0100

    edited footer

--- src/app/datahandler.service.ts
+++ src/app/datahandler.service.ts
@@ -9,8 +9,8 @@ export class DatahandlerService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    //return this.http.get("https://raw.githubusercontent.com/profanta3/mountray/master/src/server/dataset.json?token=AKH5PJ67P7W5H3FOWCTAWLTBZXIGC");
+    //return this.http.get("https://raw.githubusercontent.com/profanta3/mountray/master/src/server/dataset.json?token=GHSAT0AAAAAABRFYKLOXBU4TRIPXYJMLAUWYP2TIKA");
     //return this.http.get("http://127.0.0.1:3000/mountains");
-    return this.http.get("http://10.211.55.4:3000/mountains");
+    return this.http.get("http://10.211.55.4:8080/api/v1/mountains");
   }
 }
